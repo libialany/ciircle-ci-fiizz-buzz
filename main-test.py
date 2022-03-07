0927a52e11bad1cf03af7a9fbddf6db8e474077a
libialany <elibialany@gmail.com>
2022-03-07 13:59:04 -0400

    Add test to make sure

--- main-test.py
+++ main-test.py
@@ -1,6 +1,7 @@
 from main import  ResponseFizzBar
 def TestResponseFizzBar():
     assert ResponseFizzBar("fizz")=="buz"
+    assert ResponseFizzBar("fuzz")=="baz"
     print('PLay FizzFuzz')
 if __name__=="__main__":
     TestResponseFizzBar()
