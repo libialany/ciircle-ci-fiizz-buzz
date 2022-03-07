38273ec8d8239fdd145f821a33487e548fc26ab3
libialany <elibialany@gmail.com>
2022-03-07 12:46:20 -0400

    first commit

---
+++ main-test.py
@@ -0,0 +1,6 @@
+from main import  ResponseFizzBar
+def TestResponseFizzBar():
+    assert ResponseFizzBar("fizz")=="buz"
+    print('PLay FizzFuzz')
+if __name__=="__main__":
+    TestResponseFizzBar()
---
+++ main.py
@@ -0,0 +1,8 @@
+def ResponseFizzBar(var1):
+    if(var1=="fizz"):
+        return "buz"
+    return "fizz"
+def FizzBar():
+    print("FizzBuzz")
+if __name__=="__main__":
+    FizzBar()
